fd36d7f6e3d2fc0c677aa37a1bfcf76047c04cdf
jeongsso <rpsha33@naver.com>
2021-01-15 18:36:34 +0900

    tst

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
 
 const app = express();
 const PORT = 3001;
 
 const db = require('./config/db.js');
 
-app.get('/', async (req, res) => {
-  const [result] = await db.query(`SELECT * from test`);
-  res.json(result);
+app.get('/', async (req: object, res: object) => {
+  console.log(123);
+
+  // const [result] = await db.query(`SELECT * from test`);
+  // res.send(res);
+  // res.json(result);
 });
 
 app.listen(PORT, () => {
